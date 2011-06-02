43f8ab96306936042a68fb8a8a2e6fba8a0d9a0a
Yasunori Endo <s37311@sendai-nct.jp>
2011-06-02 21:58:59 +0900

    modified:   device_list.c

---
+++ device_list/device_list.c
@@ -0,0 +1,23 @@
+/**************************************************
+ * PortAudio API practice
+ *   Show all available devices
+ *
+ * Wakiyama lab. of SNCT
+ *   Yasunori Endo
+ *************************************************/
+
+/* If you don't want to generate debug codes,
+ * uncomment NDEBUG macro below.
+ */
+//#define NDEBUG
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<portaudio.h>
+
+
+int main(int argc, char **argv){
+  printf("PortAudio practice - show devices\n");
+
+  return EXIT_SUCCESS;
+}
