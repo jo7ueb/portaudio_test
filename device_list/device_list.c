400bc0a148611075ff2582dcf38830e557511db3
Yasunori Endo <yasu@neppo.net>
2011-06-03 08:29:36 +0900

    デバイスの個数を表示させてみた

--- device_list/device_list.c
+++ device_list/device_list.c
@@ -17,7 +17,25 @@
 
 
 int main(int argc, char **argv){
+  PaError error_code;
+  PaDeviceIndex number_dev;
+  PaDeviceInfo *devices;
+
   printf("PortAudio practice - show devices\n");
+
+
+  /* Initialize PortAudio */
+  error_code = Pa_Initialize();
+
+  
+  /* count how many devices available */
+  printf("Inspecting how any devices.\n");
+  number_dev = Pa_GetDeviceCount();
+
+  #ifndef NDEBUG
+  printf("There're %d device(s) available\n", number_dev);
+  #endif
+
 
   return EXIT_SUCCESS;
 }
