06d2113e5c68f313e654c52f87ea0a8785adfbe0
Yasunori Endo <yasu@neppo.net>
2011-06-04 23:11:23 +0900

    今度はGUIでデバイスを表示してみようかと思う。

---
+++ device_list/gui_devices.c
@@ -0,0 +1,18 @@
+/***********************************************
+ * PortAudio & GTK+ API practice
+ *   Show all available devices with GTK+
+ *
+ * Wakiyama lab. of SNCT
+ *   Yasunori Endo
+ **********************************************/
+
+/* If you don't want to generate debug codes,
+ * uncomment NDEBUG macro below.
+ */
+//#define NDEBUG
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<portaudio.h>
+#include<gtk/gtk.h>
