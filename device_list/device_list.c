9942b569bc8389508c82e4c6bcd258aa5cf49599
Yasunori Endo <yasu@neppo.net>
2011-06-04 20:57:09 +0900

    デバイス表示プログラム完成ってことで。

--- device_list/device_list.c
+++ device_list/device_list.c
@@ -20,6 +20,7 @@
 int main(int argc, char **argv){
   PaError error_code;
   PaDeviceIndex number_dev;
+  PaHostApiIndex number_host;
   int i;
 
   printf("PortAudio practice - show devices\n");
@@ -38,7 +39,7 @@ int main(int argc, char **argv){
   #endif
 
   #ifndef NDEBUG
-  printf("Size of PaDeviceInfo: %d\n", sizeof(PaDeviceInfo));
+  printf("Size of PaDeviceInfo: %lu\n\n", sizeof(PaDeviceInfo));
   #endif
 
 
@@ -53,11 +54,45 @@ int main(int argc, char **argv){
     printf("Host API Name: %s\n", info_api->name);
     printf("Max Inputs: %d\n", info_dev->maxInputChannels);
     printf("Max Outputs: %d\n", info_dev->maxOutputChannels);
-    printf("Default Sample Rates: %lf\n", info_dev->defaultSampleRate);
+    printf("Default Sample Rates[Hz]: %lf\n", info_dev->defaultSampleRate);
+
+    if(info_dev->maxInputChannels > 0){
+      printf("Low Input Latency[ms]: %lf\n",
+	     info_dev->defaultLowInputLatency * 1000.0);
+      printf("High Input Latency[ms]: %lf\n",
+	     info_dev->defaultHighInputLatency * 1000.0);
+    }
+
+    if(info_dev->maxOutputChannels > 0){
+      printf("Low Output Latency[ms]: %lf\n",
+	     info_dev->defaultLowOutputLatency * 1000.0);
+      printf("High Output Latency[ms]: %lf\n",
+	     info_dev->defaultHighOutputLatency * 1000.0);
+    }
 
     printf("\n");
   }
  
+
+  /* get host API info */
+  number_host = Pa_GetHostApiCount();
+
+  #ifndef NDEBUG
+  printf("There're %d host API(s) available\n\n", number_host);
+  #endif
+
+  for(i=0; i<number_host; ++i){
+    const PaHostApiInfo *info = Pa_GetHostApiInfo(i);
+ 
+    printf("API #%d\n", i);
+    printf("API Name: %s\n", info->name);
+    printf("Device Count: %d\n", info->deviceCount);
+    printf("Default Input Device: %d\n", info->defaultInputDevice);
+    printf("Default Output Device: %d\n", info->defaultOutputDevice);
+
+    printf("\n");
+  }
+
 
   /* Terminate PortAudio */
   Pa_Terminate();
