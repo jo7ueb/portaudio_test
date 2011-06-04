a8d44ac8e72d861cc81bc00640b0313dc11dba54
Yasunori Endo <yasu@neppo.net>
2011-06-05 06:04:18 +0900

    Implement get_dev_info()

--- device_list/gui_devices.c
+++ device_list/gui_devices.c
@@ -50,3 +50,59 @@ typedef struct{
 static char g_labelInfo[2048];
 static int g_num_dev;
 static Devinfo *g_devices = NULL;
+
+
+
+
+/* Prepare list of devices */
+static void get_dev_info(){
+  int i;
+
+
+  /* allocate g_devices */
+  g_num_dev = Pa_GetDeviceCount();
+  g_devices = (DevInfo *)malloc(SIZE_DEVINFO * g_num_dev);
+
+  #ifndef NDEBUG
+  printf("Total devices: %d\n", g_num_dev);
+  #endif
+
+
+  /* Store info into g_devices */
+  for(i=0; i<g_num_dev; ++i){
+    const PaDeviceInfo *info_dev = Pa_GetDeviceInfo(i);
+    const PaHostApiInfo *info_api = Pa_GetHostApiInfo(info_dev->hostApi);
+    int len_dev_name, len_api_name;
+
+    len_dev_name = strlen(info_dev->name);
+    g_devices[i].name_dev = (char *)malloc(len_dev_name + 1);
+    #ifndef NDEBUG
+    printf("device: %s len: %d\n", info_dev->name, len_dev_name);
+    #endif
+
+    len_api_name = strlen(info_api->name);
+    g_devices[i].name_api = (char *)malloc(len_api_name + 1);
+    #ifndef NDEBUG
+    printf("api: %s len: %d\n", info_api->name, len_api_name);
+    #endif
+
+    g_devices[i].id_dev = i;
+
+    g_devices[i].max_inputs = info_dev->maxInputChannels;
+    g_devices[i].max_outputs = info_dev->maxOutputChannels;
+
+    g_devices[i].high_input_latency = info_dev->defaultHighInputLatency * 1000.0;
+    g_devices[i].low_input_latency = info_dev->defaultLowInputLatency * 1000.0;
+    g_devices[i].high_output_latency = info_dev->defaultHighOutputLatency * 1000.0;
+    g_devices[i].low_output_latency = info_dev->defaultLowOutputLatency * 1000.0;
+
+    g.devices[i].sample_rate = info_dev->defaultSampleRate;
+
+    strcpy(g_devices[i].name_dev, info_dev->name);
+    strcpy(g_devices[i].name_api, info_api->name);
+    #ifndef NDEBUG
+    printf("\ncopied device:%s\ncopied api:%s\n",
+	   g_devices[i].name_dev, g_devices[i].name_api);
+    #endif
+  }
+}
